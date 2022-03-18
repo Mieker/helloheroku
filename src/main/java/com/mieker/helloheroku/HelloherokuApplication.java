16ae676c5be6560c7537fb5866629bf2d5c9fe4a
Mieker <mieker@wp.pl>
2022-03-18 17:01:45 +0100

    init

---
+++ src/main/java/com/mieker/helloheroku/HelloherokuApplication.java
@@ -0,0 +1,13 @@
+package com.mieker.helloheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloherokuApplication.class, args);
+	}
+
+}
